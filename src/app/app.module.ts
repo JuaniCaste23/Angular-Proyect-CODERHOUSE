d752480675c9530f7e384f9546dbc07496c5c9dc
JuaniCaste23 <juanicaste23@gmail.com>
2022-11-17 22:35:24 -0300

    integrando segundo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MiPrimerComponenteComponent } from './components/mi-primer-componente/mi-primer-componente.component';
+import { MiSegundoComponenteComponent } from './components/mi-segundo-componente/mi-segundo-componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiPrimerComponenteComponent
+    MiPrimerComponenteComponent,
+    MiSegundoComponenteComponent
   ],
   imports: [
     BrowserModule
